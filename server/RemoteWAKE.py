6f8073df5b5519780ce6a525e31ea26f996c8032
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-02-09 06:55:18 -0500

    Create RemoteWAKE.py

---
+++ server/RemoteWAKE.py
@@ -0,0 +1,14 @@
+# RemoteWAKE Python Server
+# This server receives responses from the website, authenticates them, parses them, and if everything is in working
+# order, creates a alarm of annoyingness set by yourself. Good Luck!
+# Now I have to put a disclaimer... Kind of ridiculous... I know...
+# Disclaimer: This software is provided as is. The developers, contributors and/or maintainers of this open source project
+# are not AND will not be held responsible for any physical damage or harm caused this application/software. If you 
+# have any heart, nerve conditions and disabilities or high blood pressure, PLEASE DO NOT USE THIS SOFTWARE APPLICATION.
+# Copyright 2014 Matthew Gross (Matt Gross) (mattgross.net)(http://github.com/MatthewGross)
+# Github: http://github.com/MatthewGross/RemoteWAKE
+# Website: http://mattgross.net/projects/remotewake
+# Enjoy! Litle note: Only give this to people you trust. If you are stupid enough to throw the login on Facebook or Twitter
+# I tend to believe you deserve to be woken up with a huge alarm every 5 seconds.
+# !-- Massive Comments End!
+# !-- Software.... COMMENCE...

aac359f3e041a600d82e284d49fee7b59a5e24ba
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-02-09 12:58:05 -0500

    Create sounds.php

---
+++ website/sounds.php
@@ -0,0 +1,8 @@
+<html><head><title>Sounds</title></head>
+<body>
+  <p>One : <a href="/sounds/one.wav"></p>
+  <p>Two : <a href="/sounds/two.wav"></p>
+  <p>Three : <a href="/sounds/three.wav"></p>
+  <p>Four : <a href="/sounds/four.wav"></p>
+  <p>Five : <a href="/sounds/five.wav"></p>
+</body></html>

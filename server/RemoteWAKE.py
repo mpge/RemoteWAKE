05da16450fc8b2385f9a9c729c62f2fd061d6304
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-02-09 07:44:27 -0500

    still need to complete.

--- server/RemoteWAKE.py
+++ server/RemoteWAKE.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 
+
 # RemoteWAKE Python Server
 # This server receives responses from the website, authenticates them, parses them, and if everything is in working
 # order, creates a alarm of annoyingness set by yourself. Good Luck!
@@ -12,3 +14,77 @@
 # I tend to believe you deserve to be woken up with a huge alarm every 5 seconds.
 # !-- Massive Comments End!
 # !-- Software.... COMMENCE...
+
+import socket
+import sys
+from time import sleep # for setting alarms in advance
+from config import *
+
+# variables already set from "from config import *: HOST, PORT, BUFFER_SIZE, LISTEN_FROM
+if HOST and PORT and BUFFER_SIZE and LISTEN_FROM:
+  # configs set... let's go!
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_addr = (host, port)
+  print >>sys.stderr, 'Starting RemoteWAKE Server on %s port %s ... Listening...' % server_addr
+  sock.bind(server_addr)
+  # Listen for incoming
+  sock.listen(1)
+  
+  while True:
+    # waiting...
+    # no message required
+    connection, client_addr = sock.accept()
+    try:
+      print >>sys.stderr, 'Incoming Connection from ', client_addr
+      # define default response message
+      message = ''
+      # check if client_addr matches LISTEN_FROM, otherwise it's another site...
+      if client_addr == LISTEN_FROM:
+        # allowed IP
+        # listen for data
+        while True:
+          data = connection.recv(16)
+          print >>sys.stderr, 'received "%s"' % data
+          if data:
+            print >>sys.stderr, 'Data Received... Parsing...'
+            pairs = data.split('||') # split by '||'
+            # setup dict
+            result = {}
+            # loop through splitted data
+            for pair in pairs:
+              (key, value) = pair.split('=')
+              result[key] = value
+              print >>sys.stderr, 'Config %s set to "%s"' %(key, value)
+            print >>sys.stderr, "All Results Set... Completing Action..."
+            # check action
+            if action == "startAlarm":
+            
+            if action == "setAlarm":
+              # setting the alarm in advance... Start the countdown
+              countdown = int(float(result.get("time")) # in seconds
+              # post status to server
+              print >>sys.stderr, 'Setting Countdown at %s Seconds' % countdown
+              # countdown!
+              while True:
+                # counting down from countdown
+                if countdown > 0:
+                  countdown = (countdown-1)
+                  print >>sys.stderr, 'Alarm in %s Seconds...' % countdown
+                else:
+                  # countdown is 0
+                  print >>sys.stderr, 'Running Alarm! WAKE UP!'
+                  # run alarm
+                  run_alarm(alarm_type, how_long, from_name, from_message)
+                  # exit while loop:
+                  break
+              
+          else:
+            print >>sys.stderr, 'No More Data from', client_addr
+            break
+
+      else:
+        # send connection refused message
+        message = "Connection Refused... Your IP Address is Not Allowed to send to this server. Error: IP_CONNECTION_REFUSED"
+      
+      # send default message
+      connection.send(message)

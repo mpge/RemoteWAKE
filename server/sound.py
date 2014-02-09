085585f93cd47936565087fa296c7f94398ad77c
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-02-09 10:33:25 -0500

    Create sound.py

---
+++ server/sound.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# Sound Config File... Only change these settings if you want a custom sound (You should replace it with one of the values.
+# Also change the file on the website as well (Simply replace files for now)
+
+Sounds[1] = "sounds/one.wav"
+Sounds[2] = "sounds/two.wav"
+Sounds[3] = "sounds/three.wav"
+Sounds[4] = "sounds/four.wav"
+Sounds[5] = "sounds/five.wav"
+
+# We only recommend 5 for now. This script is just a hacked up script. I might add the ability to add unlimited amount
+# of sound files. As of now, only 5 are available within the website and server scripts.
